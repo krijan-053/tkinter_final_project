9f35a584c3f76c31434d60e5a5257e7e8b8c2ece
krijan_053 <120587254+krijan-053@users.noreply.github.com>
2023-06-06 19:09:40 +0545

    Update-1 create_table.py

--- create_table.py
+++ create_table.py
@@ -1,22 +1,37 @@
 from estd_connection import estd_connection
 
-
 def create_table():
     cursor = estd_connection()
+    try:
+        cursor.execute("DROP TABLE  DATA")
 
-    cursor.execute("DROP TABLE  DATA")
+    except:
+        sql = """
+        CREATE TABLE DATA(
+            TITLE CHAR(10),
+            FIRST_NAME CHAR(20),
+            LAST_NAME CHAR(20),
+            AGE INT,
+            STATUS CHAR(50),
+            NATIONALITY CHAR(50),
+            COMPLETED_COURSES INT,
+            COMPLETED_SEMESTERS INT     
+        )
+        """
 
-    sql = """
-    CREATE TABLE DATA(
-        TITLE CHAR(10),
-        FIRST_NAME CHAR(20),
-        LAST_NAME CHAR(20),
-        AGE INT,
-        NATIONALITY CHAR(50),
-        COMPLETED_COURSES INT,
-        COMPLETED_SEMESTERS INT
+        cursor.execute(sql)
+    else:
+        sql = """
+        CREATE TABLE DATA(
+            TITLE CHAR(10),
+            FIRST_NAME CHAR(20),
+            LAST_NAME CHAR(20),
+            AGE INT,
+            STATUS CHAR(50),
+            NATIONALITY CHAR(50),
+            COMPLETED_COURSES INT,
+            COMPLETED_SEMESTERS INT     
+        )
+        """
 
-    )
-    """
-
-    cursor.execute(sql)
+        cursor.execute(sql)
